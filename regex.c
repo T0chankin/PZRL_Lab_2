df9a15534508cc08156044d0a2b445e82408597a
T0chankin <maximdyuzhenkov@yandex.ru>
2024-12-21 06:40:50 +0300

    finished with replacement

--- regex.c
+++ regex.c
@@ -3,21 +3,65 @@
 #include <string.h>
 #include "regex.h"
 
+#define MAX_LINE_LENGTH 1024
+
 bool errorChecker(size_t argc,char **argv){
     
 }
-replacer(char **argv){
 
+void replacer(const char *filename, const char *oldText, const char *newText) {
+    FILE *file = fopen(filename, "r+");
+    char line[MAX_LINE_LENGTH];
+    char buffname[] = "temp_file.txt";
+    FILE *buff = fopen(buffname, "w");
+
+    if (file == NULL) {
+        perror("Error opening file");
+        return;
+    }
+    if (buff == NULL) {
+        perror("Error creating buff file");
+        fclose(file);
+        return;
+    }
+
+    while (fgets(line, sizeof(line), file)) {
+        char *ptr = strstr(line, oldText);
+        if (ptr != NULL) {
+            char *restOfLine = ptr + strlen(oldText);
+            fprintf(buff, "%.*s%s%s", (int)(ptr - line), line, newText, restOfLine);
+        } else {
+            fprintf(buff, "%s", line);
+        }
+    }
+
+    fclose(file);
+    fclose(buff);
+
+    if (remove(filename) != 0) {
+        perror("Error removing original file");
+        return;
+    }
+
+    if (rename(buffname, filename) != 0) {
+        perror("Error renaming temporary file");
+        return;
+    }
+
+    printf("Replace completed successfully\n");
 }
+
 
 void operator(size_t argc, char **argv){
     switch (argv[2][1])
     {
     case 'r':
         printf("flag r\n");
+        replacer(argv[1],argv[3],argv[4]);
         break;
     case 'd':
         printf("flag d\n");
+        replacer(argv[1],argv[3]," ");
         break;
     case 'i':
 
@@ -33,7 +77,7 @@ void operator(size_t argc, char **argv){
         break;
 
     default:
-    printf("ti loh\n");
+    printf("Error wrong flag\n");
         break;
     }
 }
